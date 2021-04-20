c845b0ae349e24cdff573f2efed7f99830c767fa
Martin Oywa <martinoywa2@gmail.com>
2021-04-20 11:47:08 +0300

    Add gopher project

---
+++ src/gopher/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Time: %d:%d\n", time.Now().Hour(), time.Now().Minute())
+	fmt.Println("  __      _")
+	fmt.Println("o'')}____//")
+	fmt.Println(" `_/      )")
+	fmt.Println(" (_(_/-(_/")
+
+	fmt.Printf("\nTime: %d:%d\n", time.Now().Hour(), time.Now().Minute())
+	fmt.Println("    `.-::::::-.`    ")
+	fmt.Println(".:-::::::::::::::-:.")
+	fmt.Println("`_:::    ::    :::_`")
+	fmt.Println(" .:( ^   :: ^   ):. ")
+	fmt.Println(" `:::   (..)   :::. ")
+	fmt.Println(" `:::::::UU:::::::` ")
+	fmt.Println(" .::::::::::::::::. ")
+	fmt.Println(" O::::::::::::::::O ")
+	fmt.Println(" -::::::::::::::::- ")
+	fmt.Println(" `::::::::::::::::` ")
+	fmt.Println("  .::::::::::::::.  ")
+	fmt.Println("    oO:::::::Oo     ")
+}
